7b47ec7a37a639c4d1b10f7a014315fbb5426510
Jim Duran <jimmy.duran@gmail.com>
2017-12-21 15:23:18 -0700

    start of project
    some simple scripts

---
+++ project1.py
@@ -0,0 +1,19 @@
+import readline
+
+print("hello world\n")
+
+m = [9, 14, 24]
+
+def modify(k):
+    k.append(39)
+    print("k =", k)
+
+modify(m)
+
+
+
+def banner(message, border='-'):
+    line = border * len(message)
+    print(line)
+    print(message)
+    print(line)
